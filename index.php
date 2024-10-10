8b9059f25639f8e6a65adcb82e2501d0c8fbd7ca
harounaKane <harouna.Kane@hotmail.fr>
2024-10-10 09:46:28 +0200

    affichage des article sur la page d'accueil en HTML

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+    <header class="bg-secondary p-4">
+        <h1 class="text-center">BLOG</h1>
+        <div class="text-end">
+            <a href="" class="text-light">Administration</a>
+        </div>
+    </header>
+    <main class="container-fluid">
+        <h2 class="text-success">PHP</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iusto molestiae dolores a, accusantium laborum possimus quam error dolorem ea!
+        </p>
+        <div>
+            Créé par Toto le 10/10/2024
+        </div>
+
+        <hr>
+
+        <h2 class="text-success">JS</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut iusto molestiae dolores a, accusantium laborum possimus quam error dolorem ea!
+        </p>
+        <div>
+            Créé par Tata le 05/10/2024
+        </div>
+    </main>
+    
+</body>
+</html>
